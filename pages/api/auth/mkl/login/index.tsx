7cd291b151d0a73d106a75556ddbcf0cf8783e19
Quasi <quasipersonal58@gmail.com>
2022-06-09 18:15:08 +0800

    Flx: token exposed

--- pages/api/auth/mkl/login/index.tsx
+++ pages/api/auth/mkl/login/index.tsx
@@ -1,8 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import { getSession } from "next-auth/react";
 
 export default async function MklSignIn(req: NextApiRequest, res: NextApiResponse) {
     try {
+
+        const session = await getSession({ req });
+
+        if(session === null || !session.isAdmin) {
+            throw new Error("Login api server failure.");
+        }
 
         const apiServerUrl = process.env.NODE_ENV === "development" ? "http://localhost:8030" : "https://mckismetlab.net/api";
         const response = await fetch(apiServerUrl + "/oauth2/token", {
--- pages/server/whitelist/index.tsx
+++ pages/server/whitelist/index.tsx
@@ -1,7 +1,18 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import PanelLayout from "../../../components/layout/panelLayout/PanelLayout";
+// import socketIo from "socket.io-client";
 
 export default function Whitelist() {
+
+    useEffect(() => {
+
+        // const io = socketIo("http://localhost:8030");
+
+        // io.on("message", (message: string) => {
+        //     console.log("Message: " , message);
+        // });
+
+    }, []);
 
     return (
         <div>
